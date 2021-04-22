eccba767590ac67aebfed01339361239489a9ee3
Florzo <florian.rizzo@etu.univ-amu.fr>
2021-04-22 10:17:29 +0200

    Implémentation du code entier pour faire fonctionner tout les test

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -3,7 +3,7 @@ package fr.univ_amu.iut.exercice1;
 public class HelloWorld {
     public static String hello(String s) {
         if(s == null || s.equals(""))
-            return "Hello, World!";
+            return "Hello, World !";
     return "Hello, " + s + "!";
 
     }
